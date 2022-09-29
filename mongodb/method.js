f7941a6eec3284b654792d833c056d0fbfadf191
Cong Khoa Huynh <bhuynhcongkhoa@gmail.com>
2022-09-29 21:03:54 +0700

    update...

--- mongodb/method.js
+++ mongodb/method.js
@@ -158,28 +158,7 @@ function findDocuments(query, collectionName, sort, limit = 50, aggregate = [], 
     });
 }
 
-// change Date
-function changeDate(id, collectionName) {
-    return new Promise((resolve, reject) => {
-        MongoClient.connect(CONNECTION_STRING, { useNewUrlParser: true, useUnifiedTopology: true })
-            .then((client) => {
-                const dbo = client.db(DATABASE_NAME);
-                const collection = dbo.collection(collectionName);
-                collection
-                    .find({})
-                    .forEach(function (doc) {
-                        doc.date = new Date(doc.date);
-                    })
-                    .then((result) => resolve(result))
-                    .catch((err) => reject(err))
-                    .finally(() => {
-                        client.save();
-                        client.close();
-                    });
-            })
-            .catch((err) => reject(err));
-    });
-}
+
 
 module.exports = {
     insertDocument,
@@ -189,5 +168,4 @@ module.exports = {
     deleteDocuments,
     findDocument,
     findDocuments,
-    changeDate,
 };
